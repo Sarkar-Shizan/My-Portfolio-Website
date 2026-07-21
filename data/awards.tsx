a75aa30931d6f754ed7e5b3021baf2e1b34b7340
Shizan711 <shizansarkar@gmail.com>
2026-07-22 01:34:56 +0600

    added new awards details

--- data/awards.tsx
+++ data/awards.tsx
@@ -10,6 +10,24 @@ export type AwardItem = {
 export const awards: AwardItem[] = [
 
   // New awards can be added here in the future
+  {
+    id: "deans-list-fall-2025-2026",
+    title: "Dean’s List Honors",
+    meta: "Fall 2025–2026 · GPA 3.95",
+    description:
+      "Awarded by the Faculty of Science and Technology at American International University-Bangladesh for outstanding academic performance.",
+    image: "#",
+    awardUrl: "#"
+  },
+  {
+    id: "deans-list-spring-2024-2025",
+    title: "Dean’s List Honors",
+    meta: "Spring 2024–2025 · GPA 4.00",
+    description:
+      "Awarded by the Faculty of Science and Technology at American International University-Bangladesh for outstanding academic performance.",
+    image: "#",
+    awardUrl: "#",
+  },
 
   {
     id: "deans-list-fall-2024-2025",
